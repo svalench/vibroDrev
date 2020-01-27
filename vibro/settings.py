57983eac30a1815d511cdbc018b351ea6421a981
alex <chitsalex@gmail.com>
2020-01-27 09:35:20 -0800

    fix admin static file 404

--- vibro/settings.py
+++ vibro/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 PROJECT_ROOT =os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
+STATIC_URL = '/code/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL ='/'
